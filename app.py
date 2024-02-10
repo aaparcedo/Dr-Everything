06bfa8c20147a8fe77d8eea4b191b59a3c4b539f
Mohamadm606 <125500847+Mohamadm606@users.noreply.github.com>
2024-02-10 04:44:30 -0500

    added microphone button

--- app.py
+++ app.py
@@ -2,19 +2,19 @@ import streamlit as st
 import pandas as pd
 from audio import record
 
-st.title("Dr. Everything at ya service...")
 
-st.markdown('## Whats the deal?')
+def submit():
+    st.session_state.something += st.session_state.widget
+    st.session_state.widget = ''
 
 if 'something' not in st.session_state:
     st.session_state.something = ''
 
-def submit():
-    st.session_state.something = st.session_state.widget
-    st.session_state.widget = ''
+st.title("Dr. Everything at ya service...")
 
+st.markdown('## Whats the deal?')
+
+st.button('🎙️', on_click=record)   
 st.text_input('Enter text...', key='widget', on_change=submit)
 
 st.write(f'Last submission: {st.session_state.something}')
-
-st.button('record audio', on_click=record)
