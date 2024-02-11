3ec37aee269265912078ecec35454123589e0e27
Gavin Roake <ga633493@ucf.edu>
2024-02-11 06:18:21 -0500

    syntax

--- app.py
+++ app.py
@@ -108,8 +108,8 @@ def generate_audio_from_text(text_input):
     rand_url = rogan_audio_url
     if(random.randint(1, 3) == 1):
         rand_url = lex_audio_url
-    elif(random.randint(1, 3) == 2)
-        rand_url = peter_audio_url;
+    elif(random.randint(1, 3) == 2):
+        rand_url = peter_audio_url
     # Make sure you've set your REPLICATE_API_TOKEN in your environment variables
     output = replicate.run(
         "lucataco/xtts-v2:684bc3855b37866c0c65add2ff39c78f3dea3f4ff103a436465326e0f438d55e",
